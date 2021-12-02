576029d174b9ad99497371eca501311fb6602de3
laura pircalaboiu <laura.pircalaboiu@yahoo.it>
2021-12-02 14:30:58 +0100

    add day 2 part 2

--- lib/day02.ex
+++ lib/day02.ex
@@ -28,6 +28,23 @@ defmodule Aoc2021.Day02 do
   end
 
   def part2(file \\ "input/day02.txt") do
-    5
+    parse(file)
+    |> Enum.chunk_every(2)
+    |> IO.inspect()
+    |> Enum.reduce({0, 0, 0},fn([a, b], acc) ->
+        case a do
+          "forward" ->
+                acc = {elem(acc, 0) + String.to_integer(b), elem(acc, 1) + String.to_integer(b) * elem(acc, 2), elem(acc, 2)}
+          "backward" ->
+                acc = {elem(acc, 0) - String.to_integer(b), elem(acc, 1) - String.to_integer(b) * elem(acc, 2), elem(acc, 2)}
+          "up" ->
+                acc = {elem(acc, 0), elem(acc, 1), elem(acc, 2) - String.to_integer(b)}
+          "down" ->
+                acc = {elem(acc, 0), elem(acc, 1), elem(acc, 2) + String.to_integer(b)}
+        end
+      end
+    )
+    |> Tuple.delete_at(2)
+    |> Tuple.product()
   end
 end
