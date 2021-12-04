7683726431b8c8aca0696194c6f05a453e079b05
laura pircalaboiu <laura.pircalaboiu@yahoo.it>
2021-12-04 09:12:39 +0100

    day 4 parsing

--- lib/day04.ex
+++ lib/day04.ex
@@ -1,17 +1,24 @@
-defmodule Aoc2021.Day01 do
+defmodule Aoc2021.Day04 do
   defp parse(file) do
-    File.read!(file)
-    |> String.split("\n\n")
-  end
+    parsed = File.read!(file)
+    |> String.split("\n\n", trim: true)
+    
 
-  def part1(file \\ "input/day01.txt") do
+  def part1(file \\ "input/day04.txt") do
     nums = parse(file)
-           |> Enum.at(1)
+           |> Enum.at(0)
+           |> String.split(",", trim: true)
+           |> Enum.map(&String.to_integer/1)
 
-    tables = parse(file)
+    tables = parse(file) 
+           |> Enum.drop(1
+           |> String.split("\n"))
+           |> String.split(" ")
+           |> Enum.map(&String.to_integer/1)
+
   end
 
-  def part2(file \\ "input/day01.txt") do
+  def part2(file \\ "input/day04.txt") do
     parse(file)
   end
 end
